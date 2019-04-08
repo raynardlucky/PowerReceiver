a74039f043f9e37c7817503279111ab9dfffaafd
raynardlucky <rayluck25@gmail.com>
2019-04-08 22:08:49 +0700

    Initial commit

---
+++ app/src/main/java/com/exercise/raynard/powerreceiver/MainActivity.java
@@ -0,0 +1,56 @@
+package com.exercise.raynard.powerreceiver;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private CustomReceiver mReceiver = new CustomReceiver();
+    private static final String ACTION_CUSTOM_BROADCAST = BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+//    private TextView numberText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        numberText = (TextView)findViewById(R.id.numberText);
+
+        //Power broadcast intent
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_HEADSET_PLUG);
+
+        //Custom Broadcast intent
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mReceiver, new IntentFilter(ACTION_CUSTOM_BROADCAST));
+
+        // Register the receiver using the activity context.
+        this.registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        this.unregisterReceiver(mReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+
+    public void sendCustomBroadcast(View view) {
+//        Random r = new Random();
+//        int num = r.nextInt(21);
+//        numberText.setText(num);
+        //Custom Broadcast intent
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+
+        //Send broadcast
+        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
+    }
+}
